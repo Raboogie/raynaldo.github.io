5578fde92ac8fc003f9b39df94382883cbbc2574
RykuPrime <Dynasty7@outlook.com>
2021-05-23 18:02:25 -0400

    Updated about me page

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,7 +1,16 @@
 const AboutMe = () => {
  return (
   <>
-   <h1>About Me</h1>
+   <section className="aboutMe">
+    <h1>About Me</h1>
+    <p>My name is Raynaldo Francis. I am an ambitious Java Developer versed in following established procedures
+    and working under little or no supervision. Offering technical expertise in programming analysis, application
+    analysis and design. Excellent team player with in-depth knowledge of development tolls and languages. I have
+    worked effectively with cross-functional design teams to create software solution that elevated client-side
+    experience and significantly improved overall functionality and performance for websites, android apps, and
+    desktop applications.</p>
+   </section>
+
   </>
  )
 }
